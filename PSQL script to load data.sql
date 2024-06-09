8b6aa21ee2ebe2f0913258e0133c77ff5b59283c
konstantinTheGreat <124327537+konstantinTheGreat@users.noreply.github.com>
2024-06-09 18:04:24 +0300

    Add files via upload
    changed the file a bit

--- PSQL script to load data.sql
+++ PSQL script to load data.sql
@@ -21,12 +21,12 @@ CREATE TEMP TABLE staging_rooms (
 
 --Step 2: copy the data from csv to these 2 temp tables
 
-\COPY staging_users (UserID, Username, Email, Password, ProfilePicture, Role) FROM '/Users/user/Downloads/data_set1_users.csv' DELIMITER ',' CSV HEADER;
-\COPY staging_rooms (RoomID, RoomName, CreatedBy, CreatedAt) FROM '/Users/user/Downloads/data_set2_rooms.csv' DELIMITER ',' CSV HEADER;
+\COPY staging_users (UserID, Username, Email, Password, ProfilePicture, Role) FROM 'C:\Users\Kon\Downloads\data_set1_users.csv' DELIMITER ',' CSV HEADER; --your own path to the file
+\COPY staging_rooms (RoomID, RoomName, CreatedBy, CreatedAt) FROM 'C:\Users\Kon\Downloads\data_set2_rooms.csv' DELIMITER ',' CSV HEADER;  --your own path to the file
 
 --Step 3: Now merge with the actual tables in our database
 
-INSERT INTO GuideToursApp.Users (Username, Email, Password, ProfilePicture, Role)
+INSERT INTO GuideToursApp.Users (Username, Email, Password, ProfilePicture, Role)	--if copying 4 of this lines check the spaces between SELECT FROM WHERE
 SELECT Username, Email, Password, ProfilePicture, Role
 FROM staging_users
 WHERE Email NOT IN (SELECT Email FROM GuideToursApp.Users);
@@ -37,7 +37,7 @@ FROM staging_rooms
 WHERE RoomID NOT IN (SELECT RoomID FROM GuideToursApp.Rooms);
 
 -- Step 4: clean the temp tables ( they are of no use now)
-DROP TABLE IF EXISTS staging_users;
+DROP TABLE IF EXISTS staging_users;	
 DROP TABLE IF EXISTS staging_rooms;
 
 -- do every step one by one (if inserting the whole script it does not work)
